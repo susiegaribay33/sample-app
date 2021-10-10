5a19627370b36975f1f7218170558d57e04cad95
SusieG <susiegaribay33@gmail.com>
2021-10-10 10:30:00 -0500

    add path one

--- app/controllers/example_pages_controller.rb
+++ app/controllers/example_pages_controller.rb
@@ -1,2 +1,14 @@
 class ExamplePagesController < ApplicationController
+  def path_one
+    render json: {message: "Path 1!"}
+  end
+
+  def path_two
+    render json: {message: "Path 2!"}
+  end
+
+  def path_three
+    render json: {message: "Path 3!"}
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/path_one", controller: "example_pages", action: "path_one"  
 end
