d338ca4b3df4b7493e437e1e844dccbb5266f92f
SusieG <susiegaribay33@gmail.com>
2021-10-10 10:50:25 -0500

    add path two and three

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/path_one", controller: "example_pages", action: "path_one"  
+  get "/path_two", controller: "example_pages", action: "path_two" 
+  get "/path_three", controller: "example_pages", action: "path_three" 
 end
